34ddcd48af0d698cd802e13eb83cba1f75701b5d
Jeroen Corsius <jeroen.corsius@bannerconnect.net>
2017-09-04 12:24:14 +0200

    Add SmartMeter.Loader and LoaderService which reads the translated files and deserializes them to Telegrams

---
+++ SmartMeter.Business/Loader/LoaderService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using SmartMeter.Business.Interface;
+
+namespace SmartMeter.Business.Loader {
+  public class LoaderService {
+    public void Execute() {
+      FileInfo[] translatedFiles = _ListTranslatedFiles();
+
+      foreach (FileInfo translatedFile in translatedFiles) {
+        try {
+          string serializedTelegram = File.ReadAllText(translatedFile.FullName);
+          ITelegram telegram = JsonConvert.DeserializeObject<Telegram>(serializedTelegram);
+
+          //translatedFile.Delete();
+        }
+        catch (Exception ex) {
+          Console.WriteLine($"An error occured while loading file '{translatedFile.FullName}': {ex}");
+        }
+      }
+    }
+
+    private FileInfo[] _ListTranslatedFiles() {
+      DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(Directory.GetDirectoryRoot(AppContext.BaseDirectory), "applicationdata", "smartmeter", "translated"));
+
+      //TODO: Filter for specific file extensions to prevent reading uncompleted files
+      return directoryInfo.GetFiles();
+    }
+  }
+}
---
+++ SmartMeter.Loader/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using SmartMeter.Business.Loader;
+
+namespace SmartMeter.Loader {
+  class Program {
+    static void Main(string[] args) {
+      while (true) {
+        try {
+          LoaderService loaderService = new LoaderService();
+          loaderService.Execute();
+        }
+        catch (Exception ex) {
+          Console.WriteLine($"An error occured while loading files: {ex}");
+        }
+
+        Thread.Sleep(TimeSpan.FromSeconds(10));
+      }
+    }
+  }
+}
