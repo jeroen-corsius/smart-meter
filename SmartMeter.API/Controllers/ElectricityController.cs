d049716e7a1c4fcb4ce564dcc89f9d7f5344841d
Jeroen Corsius <jeroen.corsius@gmail.com>
2018-02-04 00:47:26 +0100

    Add basic WebApi project

---
+++ SmartMeter.API/Controllers/ElectricityController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartMeter.API.Controllers {
+  [Route("api/[controller]")]
+  public class ElectricityController : Controller {
+    [HttpGet("Recent")]
+    public IEnumerable<string> GetRecent() {
+      return new string[] { "value1", "value2" };
+    }
+  }
+}
