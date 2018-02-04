a23c98b0cef07850ad1c92e56446c7ddbee17367
Jeroen Corsius <jeroen.corsius@gmail.com>
2018-02-04 01:01:43 +0100

    Implement ElectricityController endpoint

--- SmartMeter.API/Controllers/ElectricityController.cs
+++ SmartMeter.API/Controllers/ElectricityController.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using SmartMeter.Business.Interface;
+using SmartMeter.Business.Interface.Loader;
+using SmartMeter.Business.Loader;
 
 namespace SmartMeter.API.Controllers {
   [Route("api/[controller]")]
   public class ElectricityController : Controller {
     [HttpGet("Recent")]
-    public IEnumerable<string> GetRecent() {
-      return new string[] { "value1", "value2" };
+    public IActionResult GetRecent() {
+      IManageTelegram telegramManager = new TelegramManager();
+      IEnumerable<ITelegram> telegrams = telegramManager.SelectRecent();
+
+      return Ok(telegrams);
     }
   }
 }
